8384a2f8aee15fd4fa14a876fd89a79902d1336a
johnncanani <johnncanani@gmail.com>
2023-07-22 16:05:55 -0500

    se creo el activity para agregar las tarjetas

---
+++ app/src/main/java/com/example/appderecarga/activity_para_registrar_tarjeta.java
@@ -0,0 +1,14 @@
+package com.example.appderecarga;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class activity_para_registrar_tarjeta extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_para_registrar_tarjeta);
+    }
+}
