bac8fbb92401f5cbd7dee0bdbf2d9f6990fe81bc
johnncanani <johnncanani@gmail.com>
2023-07-23 22:52:34 -0500

    se agrego al spinner los datos de las empresas

--- app/src/main/java/com/example/appderecarga/activity_operacion_recarga.java
+++ app/src/main/java/com/example/appderecarga/activity_operacion_recarga.java
@@ -3,12 +3,24 @@ package com.example.appderecarga;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class activity_operacion_recarga extends AppCompatActivity {
+
+    private Spinner spinner_selecione_empresa;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_operacion_recarga);
+
+        spinner_selecione_empresa = (Spinner) findViewById(R.id.Spiner_empresa);
+
+        String[] empresas = {"Claro, Movistar, Entel, Bitel"};
+
+        ArrayAdapter <String> empresa_seleccion = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, empresas);
+        spinner_selecione_empresa.setAdapter(empresa_seleccion);
+
     }
 }
