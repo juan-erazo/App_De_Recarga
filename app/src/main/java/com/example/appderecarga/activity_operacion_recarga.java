4d55318d31fa85d0b4c8d145926b83779b4597ce
johnncanani <johnncanani@gmail.com>
2023-07-23 22:21:36 -0500

    Se desarrollo el activity pra la operacion de recarga

---
+++ app/src/main/java/com/example/appderecarga/activity_operacion_recarga.java
@@ -0,0 +1,14 @@
+package com.example.appderecarga;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class activity_operacion_recarga extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_operacion_recarga);
+    }
+}
