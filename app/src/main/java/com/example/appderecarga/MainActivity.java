5e298e32e480644c496c79efeac71665c62b8e1a
Juan <j.manuel.e.huarache@hotmail.com>
2023-07-25 10:22:25 -0500

    Creando controles para el boton Registrar del MainActivity

--- app/src/main/java/com/example/appderecarga/MainActivity.java
+++ app/src/main/java/com/example/appderecarga/MainActivity.java
@@ -1,109 +1,33 @@
 package com.example.appderecarga;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
-import android.widget.EditText;
-import android.widget.Toast;
-
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.ValueEventListener;
 
 public class MainActivity extends AppCompatActivity {
 
-    EditText et_cuen, et_clav;
-
-    Button btnCance, btnRegi;
-
-    String msj = "";
+    Button btnRegis;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        et_cuen = findViewById(R.id.txtCuenta);
-        et_clav = findViewById(R.id.txtClave);
-        btnCance = findViewById(R.id.btnIngresar);
-        btnRegi = findViewById(R.id.btnRegis);
-
-        Registrar();
-    }
-
-    public void Registrar(){
-        btnRegi.setOnClickListener(new View.OnClickListener() {
+        btnRegis = (Button) findViewById(R.id.btnRegis);
+        btnRegis.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if (validarCampo().isEmpty()) {
-                    String nom = et_cuen.getText().toString().trim();
-                    String ape = nom + ".asdads";
-                    String usu = nom + "." + nom;
-                    String con = et_clav.getText().toString().trim();
-
-                    FirebaseDatabase db = FirebaseDatabase.getInstance();
-                    DatabaseReference dbref = db.getReference(Usuario.class.getSimpleName());
-
-                    dbref.addListenerForSingleValueEvent(new ValueEventListener() {
-                        @Override
-                        public void onDataChange(@NonNull DataSnapshot snapshot) {
-                            Usuario usuario = new Usuario(nom,ape,usu,con);
-                            dbref.push().setValue(usuario);
-                            ocultarTeclado();
-
-                            Toast.makeText(MainActivity.this,
-                                    "Usuario registrado correctamente", Toast.LENGTH_SHORT).show();
-
-                            Limpiar();
-                        }
-
-                        @Override
-                        public void onCancelled(@NonNull DatabaseError error) {
-                            Toast.makeText(MainActivity.this,
-                                    "Se produjo un error al registrar", Toast.LENGTH_SHORT).show();
-                        }
-                    });
-                }
-
-            } // Cierra el OnClick
+                Siguiente();
+            }
         });
-    } // Cierra el boton registrar
-
-    public String validarCampo(){
-
-        if (et_cuen.getText().toString().trim().isEmpty()){
-            msj = "cuenta";
-            Toast.makeText(this, "Completar el campo " + msj, Toast.LENGTH_LONG).show();
-        } else if ((et_cuen.getText().toString().trim().isEmpty())) {
-            msj = "clave";
-            Toast.makeText(this, "Completar el campo " + msj, Toast.LENGTH_LONG).show();
-        } else {
-            msj = "";
-        }
-
-        return msj;
-    } // Cierra el metodo validar
-
-    public void Limpiar(){
-        et_cuen.setText("");
-        et_clav.setText("");
     }
 
-
-    public void ocultarTeclado(){
-        View view = this.getCurrentFocus();
-
-        if(view != null){
-            InputMethodManager imn = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
-            imn.hideSoftInputFromWindow(view.getWindowToken(), 0);
-        }
-    } // Cierra el metodo ocultar teclado
-
+    /* ------------------------------------ BOTON REGISTRAR ------------------------------------- */
+    public void Siguiente(){
+        Intent sig = new Intent(MainActivity.this, activity_registrar_usuario.class);
+        startActivity(sig);
+    }
 }
