7f667e18b510b8a68bc7496ef1f40365bfdee0b6
johnncanani <johnncanani@gmail.com>
2023-07-25 16:10:08 -0500

    intentando pasar de la activity menu a la de registro de tarjeta: con ERROR

--- app/src/main/java/com/example/appderecarga/activity_menu_principal.java
+++ app/src/main/java/com/example/appderecarga/activity_menu_principal.java
@@ -2,13 +2,32 @@ package com.example.appderecarga;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
 
 public class activity_menu_principal extends AppCompatActivity {
+
+    private Button registrar_tarjeta;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu_principal);
+
+        registrar_tarjeta = (Button) findViewById(R.id.btn_RegistrarTarjeta);
+
+        registrar_tarjeta.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent sig = new Intent(activity_menu_principal.this, activity_para_registrar_tarjeta.class);
+                startActivity(sig);
+            }
+        });
+
     }
+
 }
